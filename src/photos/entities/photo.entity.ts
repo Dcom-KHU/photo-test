cc523b90534b44f3223397074d8efb313a832da1
bluejoyq <bluejoy32@gmail.com>
2023-03-03 17:33:32 +0900

    엔티티 순서 변경

--- src/photos/entities/photo.entity.ts
+++ src/photos/entities/photo.entity.ts
@@ -1,6 +1,28 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export type PhotoFilter = undefined | 'cat' | 'dog' | 'dog cat';
+
+export class PhotoSrc {
+  original: string;
+  large2x: string;
+  large: string;
+  medium: string;
+  small: string;
+  portrait: string;
+  landscape: string;
+  tiny: string;
+  constructor(data: any) {
+    this.original = data.original;
+    this.large2x = data.large2x;
+    this.large = data.large;
+    this.medium = data.medium;
+    this.small = data.small;
+    this.portrait = data.portrait;
+    this.landscape = data.landscape;
+    this.tiny = data.tiny;
+  }
+}
+
 export class Photo {
   @ApiProperty({
     description: '사진 웹사이트 경로',
@@ -28,26 +50,5 @@ export class Photo {
     this.photographer_url = data.photographer_url;
     this.src = new PhotoSrc(data.src);
     this.alt = data.alt;
-  }
-}
-
-export class PhotoSrc {
-  original: string;
-  large2x: string;
-  large: string;
-  medium: string;
-  small: string;
-  portrait: string;
-  landscape: string;
-  tiny: string;
-  constructor(data: any) {
-    this.original = data.original;
-    this.large2x = data.large2x;
-    this.large = data.large;
-    this.medium = data.medium;
-    this.small = data.small;
-    this.portrait = data.portrait;
-    this.landscape = data.landscape;
-    this.tiny = data.tiny;
   }
 }
