8aa45b333e308a09cd9d4bf9b5535aef60d61332
bluejoyq <bluejoy32@gmail.com>
2023-03-03 17:26:25 +0900

    node 버전 16에 호환

--- src/photos/photos.service.ts
+++ src/photos/photos.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Photo, PhotoFilter } from './entities/photo.entity';
 import { ConfigService } from '@nestjs/config';
-
+import fetch from 'node-fetch';
 @Injectable()
 export class PhotosService {
   constructor(private configService: ConfigService) {}
